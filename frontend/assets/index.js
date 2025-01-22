d51c2d5453f5d48576a8640236a6a7873ffb520f
Dharshan <kdharshanprog@gmail.com>
2025-01-22 14:06:40 +0530

    Rook move generation and validation completed.

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -1,4 +1,4 @@
-import { validatePawnMove } from "./utils/moves.js";
+import { validatePawnMove, generateRookMoves } from "./utils/moves.js";
 $(function() {
   let selectedSquare = null;
   
@@ -36,38 +36,60 @@ $(function() {
   }
 
   function makeMove(from, to) {
-      const position = board.position();  
-      if (position[from]) {
-        const piece = position[from];
-        let moves = [];
-        switch(piece[1]){
-            case "P":
-                moves = validatePawnMove(from, piece, board);
-                for(let i=0;i<moves.length;i++){
-                    if(moves[i]== to){
-                        const newPosition = {...position};
-                        newPosition[to] = newPosition[from];
-                        delete newPosition[from];          
-                        board.position(newPosition);         
-                    }
+    const position = board.position();  
+    if (position[from]) {
+      const piece = position[from];
+      let moves = [];
+      switch(piece[1]){
+        case "P":
+            moves = validatePawnMove(from, piece, board);
+            for(let i=0;i<moves.length;i++){
+                if(moves[i]== to){
+                    const newPosition = {...position};
+                    newPosition[to] = newPosition[from];
+                    delete newPosition[from];          
+                    board.position(newPosition);
                 }
-                break;
-            default:
-                console.log("Move is not allowed.")
+            }
+            break;
+        case "R":
+            moves = generateRookMoves(from, piece, board);
+            console.log("moves", moves)
+            if(moves.length == 0){
+                console.log("no moves.")
+            }
+            for(let i=0;i<moves.length;i++){
+                
+                if(position[to] != undefined){
+                    const toPosition = position[to]
+                    if(toPosition[0] == piece[0]){
+                        console.log("illegal move")
+                        return "illegal move"
+                    } 
+                }
+                if(moves[i]== to){
+                    const newPosition = {...position};
+                    newPosition[to] = newPosition[from];
+                    delete newPosition[from];          
+                    board.position(newPosition);
+                }
+            }
+            break;
+        default:
+            console.log("Move is not allowed.")
         }
-      }
+    }
   }
 
   function onChange(oldPos, newPos) {
-      console.log('Position changed:');
-      console.log('Old position:', oldPos);
-      console.log(oldPos)
-      console.log('New position:', newPos);
+    //   console.log('Position changed:');
+    //   console.log('Old position:', oldPos);
+    //   console.log(oldPos)
+    //   console.log('New position:', newPos);
   }
 
   // function to highlight possible moves when mouse moves to the square
   function onMouseoverSquare(square, piece){
-    console.log("square, piece",square, piece)
     let moves = []
     switch(piece){
         case "wP":
@@ -82,6 +104,20 @@ $(function() {
                 highlightSquare(moves[i])
             }
             break;
+        case "wR":
+            moves = generateRookMoves(square,piece,board)
+            if(moves.length != 0){
+                for(let i=0;i<moves.length;i++){
+                    highlightSquare(moves[i])
+                }
+            }
+            break;
+        case "bR":
+            moves = generateRookMoves(square,piece,board)
+            for(let i=0;i<moves.length;i++){
+                highlightSquare(moves[i])
+            }
+            break;        
         default:
             console.log("square is empty.")
     }
--- frontend/assets/utils/moves.js
+++ frontend/assets/utils/moves.js
@@ -1,8 +1,6 @@
 import { extractPosition, fileDict } from "./utils.js";
 import { keyDict } from "./utils.js";
 
-const validMoves = {}
-
 function detectMoves(board,selectedSquare){
   const piece = board[selectedSquare];
   switch (piece[1]){
@@ -17,10 +15,8 @@ function detectMoves(board,selectedSquare){
 function validatePawnMove(position, piece, board){
   const moves = new Array();
   const boardPosition = board.position();
-  console.log("piece", piece, position[1])
     if(piece == "wP"){
       const value = parseInt(position[1]) + 1;
-      console.log("value", parseInt(position[1]))
       const pieceString = position[0] + value.toString();
       if(boardPosition[pieceString] == undefined){
         moves.push(pieceString);
@@ -46,20 +42,85 @@ function validatePawnMove(position, piece, board){
         }            
       }
     }
-    if(position[1] == "2"){
+    if(position[1] == "2" && piece=="wP"){
       const value = parseInt(position[1]) + 2;
       const pieceString = position[0] + value.toString();
       moves.push(pieceString);      
-    }else if(position[1] == "7"){
+    }else if(position[1] == "7" && piece == "bP"){
       const value = parseInt(position[1]) - 2;
       const pieceString = position[0] + value.toString();
       moves.push(pieceString); 
     }
-    console.log("moves",moves)
   return moves;
 }
 
-// function validateRookMove()
+function generateRookMoves(position,piece, board){
+  const moves = new Array();
+  const boardPosition = board.position();
+  generateVerticalRookMove(position[0] + (parseInt(position[1])+1).toString(), boardPosition, moves, "up", piece);
+  if(parseInt(position[1]) >= 1 && parseInt(position[1]) < 8){
+    generateVerticalRookMove(position[0] + (parseInt(position[1])+1).toString(), boardPosition, moves, "up", piece);
+  }
+  if(parseInt(position[1]) > 1 && parseInt(position[1]) <= 8){
+    generateVerticalRookMove(position[0] + (parseInt(position[1])-1).toString(), boardPosition, moves, "down", piece);
+  }
+  if(position[0] == 'h'){
+    const nextSquare = fileDict[keyDict[position[0]] - 1] + position[1];
+    generateHorizontalRookMove(nextSquare, boardPosition, moves, "left");    
+  }else if(position[0] == 'a'){
+    const nextSquare = fileDict[keyDict[position[0]] + 1] + position[1];
+    generateHorizontalRookMove(nextSquare, boardPosition, moves, "right");
+  }else{
+    const rightSquare = fileDict[keyDict[position[0]] + 1] + position[1];
+    generateHorizontalRookMove(rightSquare, boardPosition, moves, "right");
+    const leftSquare = fileDict[keyDict[position[0]] - 1] + position[1];
+    generateHorizontalRookMove(leftSquare, boardPosition, moves, "left")
+  }
+  return moves;
+}
 
-export { detectMoves, validatePawnMove }
+function generateVerticalRookMove(position, board, moves, movement, piece){
+  if(parseInt(position[1]) == 8 || parseInt(position[1])== 1){
+    moves.push(position);
+    return moves;
+  }
+  if(board[position] != undefined){
+    if(board[position][0] != piece[0]){
+      moves.push(position)
+    }
+    return moves;
+  }
+  moves.push(position);
+  if(movement == "up"){
+    generateVerticalRookMove(position[0] + (parseInt(position[1])+1), board, moves, "up", piece);
+  }else if(movement == "down"){
+    generateVerticalRookMove(position[0] + (parseInt(position[1])-1), board, moves, "down", piece)
+  }
+}
 
+function generateHorizontalRookMove(position, board, moves, movement){
+  if(board[position] != undefined){
+    moves.push(position);
+    return moves;
+  }
+  if(movement == "left"){
+    if(position[0] == 'a'){
+      moves.push(position);
+      return moves;
+    }
+    const skip = fileDict[parseInt(keyDict[position[0]]) - 1] + position[1];
+    generateHorizontalRookMove(skip, board, moves, "left")
+  }
+  if(movement == "right"){
+    if(position[0] == 'h'){
+      moves.push(position);
+      return moves;
+    }
+    const skip = fileDict[parseInt(keyDict[position[0]]) + 1] + position[1];
+    generateHorizontalRookMove(skip, board, moves, "right")
+  }
+  
+  moves.push(position);
+}
+
+export { detectMoves, validatePawnMove, generateRookMoves }
